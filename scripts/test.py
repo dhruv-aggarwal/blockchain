15ae46abf72bd2912bd4b695ab62ee3e0e354edf
DHRUV AGGARWAL <dhruv.aggarwal@practo.com>
2018-01-28 18:29:06 +0530

    Add a function to save block to file

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 from block import Block
 from datetime import datetime
+import os
+from app.managers.common import get_data_directory
 
 
 def initialize_block():
@@ -11,3 +13,20 @@ def initialize_block():
     block_data['prev_hash'] = None
     block = Block(block_data)
     return block
+
+
+def save_to_file():
+    # blockchain_dir is the directory where the each block is stored locally
+    # in the file system.
+    # If the folder does not exist, need to create it.
+    directory = get_data_directory()
+    if not os.path.exists(directory):
+        # make chaindata dir
+        os.mkdir(directory)
+    # if the folder is empty, initialise the blockchain. Else add to it.to
+    # TODO: Add to an existing chain
+    # TODO: Save block to a file
+    if os.listdir(directory) == []:
+        # create first block
+        first_block = initialize_block()
+        # first_block.save()
