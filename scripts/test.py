c7e104985fc9e4810067a03ece578d8d87916dde
DHRUV AGGARWAL <dhruv.aggarwal@practo.com>
2018-02-04 12:48:14 +0530

    Update test cases

--- scripts/test.py
+++ scripts/test.py
@@ -1,36 +1,80 @@
 from app.models.block import Block
-from datetime import datetime
-import os
-from app.managers.common import get_data_directory, calculate_hash
-from app.managers.block import save_block
+from app.models.chain import Chain
+from static.test import test_data
 
+block_zero_dir = test_data[0]
+block_one_dir = test_data[1]
+block_two_dir = test_data[2]
+block_three_dir = test_data[3]
+block_three_later_in_time_dir = test_data[4]
 
-def initialize_block():
-    # index zero and arbitrary previous hash
-    block_data = {}
-    block_data['index'] = 0
-    block_data['timestamp'] = datetime.now()
-    block_data['data'] = 'First block data'
-    block_data['prev_hash'] = None
-    block_data['hash'] = calculate_hash(
-        0, None, block_data['data'], block_data['timestamp'], 0
-    )
-    block = Block(block_data)
-    return block
+###########################
+#
+# Block time
+#
+###########################
 
+block_zero = Block(**block_zero_dir)
+another_block_zero = Block(**block_zero_dir)
+assert block_zero.is_valid()
+assert block_zero == another_block_zero
+assert not block_zero != another_block_zero
 
-def save_to_file():
-    # blockchain_dir is the directory where the each block is stored locally
-    # in the file system.
-    # If the folder does not exist, need to create it.
-    directory = get_data_directory()
-    if not os.path.exists(directory):
-        # make chaindata dir
-        os.mkdir(directory)
-    # if the folder is empty, initialise the blockchain. Else add to it.to
-    # TODO: Add to an existing chain
-    # TODO: Save block to a file
-    if os.listdir(directory) == []:
-        # create first block
-        first_block = initialize_block()
-        save_block(first_block)
+block_one = Block(**block_one_dir)
+another_block_one = Block(**block_one_dir)
+assert block_one.is_valid()
+assert block_one == another_block_one
+assert not block_one != another_block_one
+
+block_two = Block(**block_two_dir)
+another_block_two = Block(**block_two_dir)
+assert block_two.is_valid()
+assert block_two == another_block_two
+assert not block_two != another_block_two
+
+block_three = Block(**block_three_dir)
+another_block_three = Block(**block_three_dir)
+assert block_three.is_valid()
+assert block_three == another_block_three
+assert not block_three != another_block_three
+
+#####################################
+#
+# Bringing Chains into play
+#
+#####################################
+blockchain = Chain([block_zero, block_one, block_two])
+assert blockchain.is_valid()
+assert len(blockchain) == 3
+
+empty_chain = Chain([])
+assert len(empty_chain) == 0
+empty_chain.add_block(block_zero)
+assert len(empty_chain) == 1
+empty_chain = Chain([])
+assert len(empty_chain) == 0
+
+another_blockchain = Chain([block_zero, block_one, block_two])
+assert another_blockchain.is_valid()
+assert len(another_blockchain) == 3
+
+print another_blockchain, blockchain
+
+assert blockchain == another_blockchain
+assert not blockchain != another_blockchain
+assert blockchain <= another_blockchain
+assert blockchain >= another_blockchain
+assert not blockchain > another_blockchain
+assert not another_blockchain < blockchain
+
+blockchain.add_block(block_three)
+assert blockchain.is_valid()
+assert len(blockchain) == 4
+assert not blockchain == another_blockchain
+assert blockchain != another_blockchain
+assert not blockchain <= another_blockchain
+assert blockchain >= another_blockchain
+assert blockchain > another_blockchain
+assert another_blockchain < blockchain
+
+blockchain.save()
